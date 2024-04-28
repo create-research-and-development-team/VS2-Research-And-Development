88658a58a7d30e15dd9325b1d62e2ca2921ea5ff
PriestOfFerns <timofeydenin@gmail.com>
2024-04-28 18:47:52 +0300

    :sparkles: fix testblock

--- common/src/main/kotlin/org/valkyrienskies/vs_rnd/block/TestBlock.kt
+++ common/src/main/kotlin/org/valkyrienskies/vs_rnd/block/TestBlock.kt
@@ -1,6 +1,7 @@
 package org.valkyrienskies.vs_rnd.block
 
 import net.minecraft.core.BlockPos
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
@@ -9,11 +10,12 @@ import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.BlockHitResult
 import org.joml.Vector3i
+import org.valkyrienskies.core.api.ships.LoadedServerShip
 import org.valkyrienskies.core.api.ships.ServerShip
 import org.valkyrienskies.core.api.ships.datastructures.ShipConnDataAttachment
 import org.valkyrienskies.core.api.ships.getAttachment
-import org.valkyrienskies.core.impl.datastructures.ShipConnDataAttachmentImpl
 import org.valkyrienskies.mod.common.getShipManagingPos
+import org.valkyrienskies.mod.common.shipObjectWorld
 
 
 class TestBlock(properties: Properties) : Block(properties) {
@@ -29,11 +31,16 @@ class TestBlock(properties: Properties) : Block(properties) {
 
 
         if (level.isClientSide) return InteractionResult.SUCCESS
-        val shipThisIsIn = level.getShipManagingPos(pos) as ServerShip
-        println(shipThisIsIn)
-        val pocket = shipThisIsIn.getAttachment<ShipConnDataAttachmentImpl>()?.getAirPocketIdFromPoint(Vector3i(pos.x,pos.y,pos.z))
-        println(shipThisIsIn.getAttachment<ShipConnDataAttachmentImpl>())
-        println(pocket)
+        val unloaded = level.getShipManagingPos(pos) ?: return InteractionResult.SUCCESS
+        val shipThisIsIn = level.shipObjectWorld.loadedShips.getById(unloaded.id) as LoadedServerShip
+        val pocket = shipThisIsIn.getAttachment<ShipConnDataAttachment>()?.getAirPocketFromPoint(Vector3i(pos.x,pos.y+1,pos.z)) ?: return InteractionResult.SUCCESS
+        pocket.pocket.forEach { t, u ->
+            print(t)
+            print(" ")
+            print(u)
+            println()
+        }
+
 
         return InteractionResult.SUCCESS
     }
